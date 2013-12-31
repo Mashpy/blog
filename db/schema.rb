23712bfcd04d06b09fec085451b2336305f9e199
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-12-31 15:44:18 +0600

    testing mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131231091904) do
+ActiveRecord::Schema.define(version: 20131231094347) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
